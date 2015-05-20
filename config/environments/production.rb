dbf41098b209461f4dcfc93b4b045c061bb99879
Oleg Cherkasky <old_juggernaut@hotmail.com>
2015-05-21 01:32:02 +0300

    modified production file so glyphicons show up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,6 +4,9 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  #this is so that the glyphicons work
+  config.asssets.compile = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
